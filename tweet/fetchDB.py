3111de3eeae1166cdf4a3e4f8f079463b58a5605
Zephyr <274432644@qq.com>
2021-05-05 17:01:11 +0800

    test fetch

--- tweet/fetchDB.py
+++ tweet/fetchDB.py
@@ -1,9 +1,12 @@
 
 from utils import get_db
+import os
+
+cmd = 'curl -X GET http://admin:admin:127.0.0.1:8787/'
+d = os.system(cmd)
 
 
 
-size = comm.get_size()
 
 url = 'http://127.0.0.1:8787'
 user = "admin"
@@ -13,8 +16,10 @@ dbname = 'newdb'
 db = get_db(url, user, pw, dbname)
 
 
-doc = db[str(0)]
-doc['id']
-print(str(doc['text']))
+print(len(db),"docs in the database")
 
-print(len(db))
+for i in len(db):
+    doc = db[str(0)]
+    doc['id']
+    print(str(doc['text']))
+
