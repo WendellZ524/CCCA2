9e73427ddf376bdbcf0c9b270bb36fba87189718
Stephen <stephen.luan@hotmail.com>
2021-05-10 00:05:02 +1000

    city name

--- tweet/utils.py
+++ tweet/utils.py
@@ -1,7 +1,12 @@
 import tweepy
 import re
 import couchdb
+import json
+import os
 from listener import CustomStreamListener
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+from shapely.geometry.multipolygon import MultiPolygon
 
 
 def get_api(consumer_key, consumer_secret, access_token, access_token_secret):
@@ -46,3 +51,23 @@ def stream_tweet(api, bounding_box, db, language=['en'], keyword=None, listener=
     stream.filter(track=keyword, locations=bounding_box, languages=language)
 
 
+def get_city_name(coord, filepath='../data/income_geo.json'):
+    with open(filepath, 'r', encoding='utf8') as f:
+        grid_info = json.load(f)
+    names = []
+    polys = []
+    features = grid_info['features']
+    for feature in features:
+        name = feature['properties']['name']
+        poly = Polygon(feature['geometry']['coordinates'][0])
+        names.append(name)
+        polys.append(poly)
+    point = Point(coord)
+
+    for n, p in zip(names, polys):
+        if p.intersects(point):
+            return n
+
+
+
+print(get_city_name((133.88362, -23.69748)))
