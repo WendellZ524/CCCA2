950ee963235430e0252b62147be519f858a8338c
Zephyr <274432644@qq.com>
2021-05-21 21:11:21 +1000

    aurin data

--- tweet/Aurin2DB.py
+++ tweet/Aurin2DB.py
@@ -8,19 +8,22 @@ dbname = "aurin_data"
 
 
 # read Aurin data from local, upload aurin datas to DB
-def get_aurin_json():
-    theme = "greater_population_goe.json"
-    myjson = load_json(theme)
-    update_db(dbname,"Aurin_pop",myjson)
-    print(theme,"uploaded ")
+def get_aurin_json(theme):
 
+    if theme == "greater_population_goe.json":
+        myjson = load_json(theme)
+        print("try to save aurin data")
+        update_db(dbname,"Aurin_pop",myjson)
+        print(theme,"uploaded ")
 
-    theme = "greater_Income_goe2.json"
-    myjson = load_json(theme)
-    id = "Aurin_income"
-    update_db(dbname,id,myjson)
-    print(theme,"uploaded ")
+    elif theme == "greater_Income_goe2.json" :
+        myjson = load_json(theme)
+        id = "Aurin_income"
+        print("try to save aurin data")
+        update_db(dbname,id,myjson)
+        print(theme,"uploaded ")
 
 if __name__ == '__main__':
-    get_aurin_json()
+    #get_aurin_json("greater_population_goe.json")
+    get_aurin_json("greater_Income_goe2.json")
     pass
